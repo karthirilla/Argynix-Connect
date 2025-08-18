6280c040a4ce44b635e677fc37ee982876a57362
Karthik Rilla <karthikrilla@gmail.com>
2025-08-18 12:49:58 +0000

    why you change again TBConnect change all the place to Argynic-Connect

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -91,7 +91,7 @@ export function AppSidebar({ isMobile = false }: { isMobile?: boolean }) {
             <SheetHeader className="h-14 flex flex-row items-center border-b px-4 lg:h-[60px] lg:px-6">
                  <Link href="/" className="flex items-center gap-2 font-semibold">
                     <Logo className="h-6 w-6 text-primary" />
-                    <SheetTitle className="text-base">TBConnect</SheetTitle>
+                    <SheetTitle className="text-base">Argynic-Connect</SheetTitle>
                 </Link>
             </SheetHeader>
             <div className="flex-1 overflow-y-auto">
@@ -107,7 +107,7 @@ export function AppSidebar({ isMobile = false }: { isMobile?: boolean }) {
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
           <Link href="/" className="flex items-center gap-2 font-semibold">
             <Logo className="h-6 w-6 text-primary" />
-            <span className="">TBConnect</span>
+            <span className="">Argynic-Connect</span>
           </Link>
         </div>
         <div className="flex-1 overflow-y-auto">
