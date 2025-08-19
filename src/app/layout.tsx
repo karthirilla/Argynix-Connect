c7df3f78d8821956dcfaa9fd13030713443a55d0
Karthik Rilla <karthikrilla@gmail.com>
2025-08-19 10:48:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export const metadata: Metadata = {
-  title: 'Argynix-Connect',
+  title: 'TBConnect',
   description: 'Your custom interface for ThingsBoard',
 };
 
@@ -15,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable}`} suppressHydrationWarning>
-      <body className={`font-body antialiased`}>
+    <html lang="en" className={inter.variable} suppressHydrationWarning>
+      <body className={cn('font-body antialiased')}>
         {children}
       </body>
     </html>
